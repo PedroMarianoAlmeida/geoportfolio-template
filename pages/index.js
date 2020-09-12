45df523a9f63ee1d155d05539c36c93a3ddc8a4d
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-12 00:43:55 -0300

    GoogleMaps centered and with on hover zoon

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import GoogleMapComponent from "../components/GoogleMaps/GoogleMaps";
 
 const Home = () => {
-  return ( 
-    
-    <div id='index-background'>  
-      
-      <div id='map-container'>
-        {/* */}<GoogleMapComponent defaultZoom={2} defaultLat={10} defaultLng={0} />
-      </div>
-      <h1 className='fixed-bottom text-center text-white'>Where I have been</h1>
+  return (
+    <>
+    <div id='index-background-container'>
     </div>
-  
-   );
+      <div id='index-background'>
+        <div id='map-container'>
+          {/**/}<GoogleMapComponent defaultZoom={0} defaultLat={25} defaultLng={15}/>
+        </div>
+      </div>
+    </>
+    
+  );
 }
- 
+
 export default Home;
