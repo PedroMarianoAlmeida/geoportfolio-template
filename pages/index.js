14f5fed2b2a9ed07fed8991408ad7c13bc81032b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-16 16:13:18 -0300

    Lateral text included - and some layout adjustments

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@ import GoogleMapComponent from "../components/GoogleMaps/GoogleMaps";
 const Home = () => {
   return (
     <>
-    <div id='index-background-container'>
-    </div>
+      <div id='index-background-container'></div>
+    
       <div id='index-background'>
+        <div id='text-left' className='vertical-text'>Behind</div>
+        <div id='text-right'className='vertical-text'>the lens</div>
         <div id='map-container'>
-          {/**/}<GoogleMapComponent defaultZoom={0} defaultLat={25} defaultLng={15}/>
+          {/*<GoogleMapComponent defaultZoom={0} defaultLat={25} defaultLng={15}/>*/}
         </div>
+
+    
       </div>
     </>
     
