8efc44372d193bf911a49d1dfc6100c17288b8ed
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-20 16:23:45 -0300

    Full mode map

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import GoogleMapComponent from "../components/GoogleMaps/GoogleMaps";
 
 const Home = () => {
+  const [fullMap, setFullMap] = React.useState(false)
+
+  const toggleFullMapMode = () => {
+    console.log('teste')
+    setFullMap(!fullMap);
+  }
+  
   return (
     <>
-      <div id='index-background-container'></div>
-    
-      <div id='index-background'>
-        <div id='text-left' className='vertical-text'>Behind</div>
-        <div id='text-right'className='vertical-text'>the lens</div>
-        <div id='map-container'>
-          {/*<GoogleMapComponent defaultZoom={0} defaultLat={25} defaultLng={15}/>*/}
-        </div>
+      <header id='navbar'>
+        <span id='navbar-logo'>My Logo</span>       
+        <span id='full-map-mode-button' onClick={toggleFullMapMode}>Full Map Mode</span>  
+      </header>
 
-    
+      <div id='index-background-container'></div>
+
+      <div className='horizontal-text'>Behind</div>
+
+      <div id={fullMap ? 'full-mode-index-background' : 'index-background'}>
+        <div id='text-left' className='vertical-text'>Behind</div>
+        <div id='text-right' className='vertical-text'>the lens</div>
+        <div id={fullMap ? 'full-mode-map-container' : 'map-container'}>
+          <GoogleMapComponent defaultZoom={0} defaultLat={25} defaultLng={15}/>
+        </div>
       </div>
+
+      <div className='horizontal-text'>the lens</div>
     </>
-    
+
   );
 }
 
